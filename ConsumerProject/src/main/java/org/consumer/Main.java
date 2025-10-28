795711baad219a0e45b42c242d61b92c39fa3744
Ethan Hanks <ethanjhanks99@gmail.com>
2025-10-27 19:02:12 -0600

    added some options

--- ConsumerProject/src/main/java/org/consumer/Main.java
+++ ConsumerProject/src/main/java/org/consumer/Main.java
@@ -1,7 +1,32 @@
-package org.example;
+package org.consumer;
 
-public class Main {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "consumer", mixinStandardHelpOptions = true,
+        version = "Consumer 1.0",
+        description = "Processes widget requests from an S3 bucket.")
+public class Main implements Callable<Integer> {
+
+
+    @Option(names = {"-wb", "--widget-bucket"}, required = true, description = "Name of the S3 bucket that will contain requests (default=null)")
+    private String widgetBucket;
+
+    @Option(names = {"-dwt", "--dynamodb-widget-table"}, description = "Name of the DynamoDB table that holds widgets (default=null)")
+    private String dynamodbWidgetTable;
+
+
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    int exitCode = new CommandLine(new Main()).execute(args);
+    System.exit(exitCode);
     }
 }
